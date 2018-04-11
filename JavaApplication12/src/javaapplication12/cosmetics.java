9bd8fa22591f39814623db28af61b1312cfb5edd
Administrator <Administrator@192.168.1.102>
2018-04-11 13:17:21 +0700

    cosmetics

---
+++ JavaApplication12/src/javaapplication12/cosmetics.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication12;
+
+//import java.util.Scanner;
+abstract class Cosmetics {
+    
+    public abstract void cosmetics();
+     
+ //System.out.println("\t 1.Lipstick\n\t 2.Under tone\n\t 3.Eyeshadow\n\t 4.Primer\n\t 5.Concealer");
+    //}
+   
+    public void brand(int a){
+               System.out.println("\t 1.Maybelline\n\t 2.Revon\n\t 3.L'OREAL");
+    }         
+   
+   // public void
+ 
+   
+
+    
+  }
+
+ //System.out.println("*** Type of lipstick ***");
+                //System.out.println("1.Cream lipstick\n2.Matte lipstick\nlip Gloss\nlip blam");
+               // System.out.print("Choose your type :");
