f52d8ab375f91a9a37006c32a512bdc28f10fb55
Administrator <Administrator@192.168.1.102>
2018-04-11 13:05:06 +0700

    main

---
+++ JavaApplication12/src/javaapplication12/login.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package project;
+
+//import java.util.Scanner;
+public class login {
+    public static void main(String[] args) {
+     
+       for (int i=1; i<=40; i++){ 
+            System.out.print("-");
+        }
+     System.out.println();
+     System.out.println("*=*=*=*=*=* Welcome to Shops *=*=*=*=*=*");
+       for (int i=1; i<=40; i++){ 
+            System.out.print("-");
+     }
+     System.out.println();
+     
+     bow m = new bow();
+     m.login();
+     for (int i=1; i<=40; i++){ 
+            System.out.print("-");
+        }
+     System.out.println();
+     
+     //Cosmetics d = new Cosmetics();
+     //d.cosmetics();
+     //Scanner want = new Scanner(System.in);
+     //System.out.println("What do you want? ");
+     //int w = want.nextInt();
+     //d.brand();
+     
+     //m.menu();
+     
+     //Scanner kb = new Scanner(System.in);
+     //System.out.print("What do you want?");
+     
+    }
+    
+}
